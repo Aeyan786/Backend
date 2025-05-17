5427da60641e26a607f8ed0656f04f30bcb7cdb3
Aeyan-Raza-pk <www.aeyanraza65@gmail.com>
2025-05-17 13:01:53 +0500

    add files

--- Database/Database.js
+++ Database/Database.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const MONGODB_URI = "mongodb+srv://admin:admin@cluster0.1etf0.mongodb.net/";
+      const  MONGODB_URI = "mongodb+srv://admin:admin@cluster0.1etf0.mongodb.net/";
 
-        mongoose.connect(MONGODB_URI)
+      await  mongoose.connect(MONGODB_URI)
 
         mongoose.connection.on("connected", () => {
             console.log("MongoDb Connected")
