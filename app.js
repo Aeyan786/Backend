fbdfe6dc731ca95b02418cf368a6e8674459da33
Aeyan-Raza-pk <www.aeyanraza65@gmail.com>
2025-05-17 13:12:05 +0500

    add files

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-import express from "express"
-import { connectDB } from "./Database/Database.js"
+import express from "express";
+import { connectDB } from "./Database/Database.js";
 import userRoutes from "./router/routes.js";
-import cors from "cors"
+import cors from "cors";
 
+const app = express();
+const PORT = 3000;
 
-const app = express()
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use("/app", userRoutes);
 
-const PORT = 3000
+// Connect to DB then start server
+const startServer = async () => {
+  await connectDB();
 
-// Middle Ware
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
 
-app.use(express.json())
-app.use("/app", userRoutes)
-app.use(cors())
-
-connectDB()
-
-app.listen(PORT, () => {
-    console.log("Server is running")
-})
+startServer();
